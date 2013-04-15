ba0b8c194f8ffd1194182e6ca868fe51f34c6248
vaibhav1997 <taketimebutnone@gmail.com>
2013-04-15 16:39:24 +0530

    Game

---
+++ adventure.js
@@ -0,0 +1,65 @@
+confirm ("Are you ready for the GAME?");
+alert ("This game is rated +4");
+
+// well the game starts here
+
+console.log("Once upon a time, there was a kid who loved cars and computer so much. He was charming and quirky. One day he was wandering around and enjoying nature, unfortunately he lost his was!");
+console.log("He was confused! There were two paths. One named 'Right' and the other named 'Left'.")
+
+//you can add images if you want to.
+
+var ans = prompt("Which one do you choose?")
+if (ans === "right")
+{
+	console.log("You move on and meet with two other paths named 'Right' and 'Wrong'.'");
+}
+else 
+{
+	console.log("Its a dead end! Please go back.");
+
+ confirm ("Are you ready to try again?")
+console.log("Once upon a time, there was a kid who loved cars and computer so much. He was charming and quirky. One day he was wandering around and enjoying nature, unfortunately he lost his was!");
+console.log("He was confused! There were two paths. One named 'Right' and the other named 'Left'.")
+
+//you can add images if you want to. 
+var ans = prompt("Which one do you choose?")
+if (ans === "right")
+{
+	console.log("You move on and meet with two other paths named 'Right' and 'Wrong'.'");
+}
+else 
+{
+	console.log("Its a dead end! Please go back.");
+}}
+
+
+var finale = prompt("Which path do you choose?");
+if (finale = "right")
+{
+	console.log("Hooray! You're home. Home sweet home.")
+}
+else
+{
+	console.log("Oops! You just went fell down a never ending pit!")
+}
+
+//its the feedback
+var feedback = prompt("Please rate the game from a scale of 1 o 10.");
+
+if (feedback > 5)
+{
+	console.log("Thank You for playing. Well you could actually contribute more to this.")
+}
+else
+{
+	console.log("Don't like it. Why don't you contribute?")
+}
+
+
+
+
+
+
+
+
+
